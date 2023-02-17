8cfadcab38fd8e3ce31bd7a8c8f1857c3902e2a1
seriadi12 <119417227+seriadi12@users.noreply.github.com>
2023-02-17 20:44:25 +0100

    Aufgabe 3
    Hier ist Aufgabe 3 a)

---
+++ Aufgabe 3.R
@@ -0,0 +1,34 @@
+# Funktionen-R-Skript 1 fuer Aufgabe 3
+
+# a)
+
+my_afunktion <- function(x){
+  a <- summary(x) 
+  
+  b <- sd(x,na.rm = TRUE) 
+  
+  c <- var(x,na.rm = TRUE)
+  
+  d <- IQR(x,na.rm = TRUE)
+  
+  e <- max(x,na.rm = TRUE)-min(x,na.rm = TRUE)
+  
+  print(a) # Min., Max., Quartile, Median, Mittelwert ausgeben
+  print(b) # Standardabweichung ausgeben
+  print(c) # Varianz ausgeben
+  print(d) # Interquartilsabstand
+  print(e) # Spannweite
+  
+  # a,b,c,d,e nacheinander ausgegeben
+}
+
+### oder mit describe Funktion, benoetigt "psych" Paket
+
+my_afunktion2 <- function(x){
+  a <- describe(x,IQR = TRUE) 
+  
+  print(a) # berechnet Stichprobenumfang, Mittelwert, Median, Min., Max.,
+           # Standardabweichung, Interquartilsabstand, und Spannweite
+  }
+
+
