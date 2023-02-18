eef63d4bec230642480e2806cb4a0b81ba56ce92
Shaban92 <76272253+Shaban92@users.noreply.github.com>
2023-02-18 22:55:32 +0100

    Update aufgabe-1-data-sim.R
    1. ich empfehle, dass wir möglicherweise auf die For-Schleife im Zeile 45 verzichten, indem wir sie durch eine Funktion von der Apply,-Familie ersetzen.
    
    2. die W'keit, dass jemand Math-LK gehabt hat, würde ich vorschlagen, einfach zufällig auszuwählen, dass die alle vier Studiengänge gleiche Verteilung haben.

--- aufgabe-1-data-sim.R
+++ aufgabe-1-data-sim.R
@@ -37,41 +37,42 @@ sim_data = function(N){
   # Waehle nun aus dem Pool zufaellige Studienfaecher N Mal aus:
   subject <- sample(subject_pool, N, replace = TRUE, prob = c(3/9, 3/9, 2/9, 1/9)) 
   
-  # 4 - Interesse an Mathematik Spalte:
+  # 4 - Interesse an Mathematik und an Programmierung Spalten gleichzeitig
   # Der Zusammenhang dabei ist frei waehlbar. Idee: 
   # Mathematik >> Statistik > Informatik == Data Science (absteigendes Interesse)
-  maths_rating <- NULL
-  # Waehle aus Effizienzgruenden eine for-Schleife:
-  for(i in 1:N){
-    # Versuche alles moeglichst aehnlich zu verteilen:
-    # Wenn das Individuum Mathematik studiert:
-    if(subject[i] == "Mathematik"){
-      maths_rating[i] <- sample(1:7, 1, replace = TRUE, 
-                         prob = c(0.05, 0.05, 0.05, 0.1, 0.2, 0.3, 0.25))
-    }
-    # Wenn das Individuum Statistik studiert:
-    if(subject[i] == "Statistik"){
-      maths_rating[i] <- sample(1:7, 1, replace = TRUE, 
-                         prob = c(0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2))
-    }
-    # Wenn das Individuum Informatik/ Data Science studiert:
-    if(subject[i] == "Informatik" || subject[i] == "Data Science"){
-      maths_rating[i] <- sample(1:7, 1, replace = TRUE, 
-                         prob = c(0.1, 0.1, 0.1, 0.15, 0.2, 0.2, 0.15))
-    }
+  # 5 - Interesse an Programmieren Spalte:
+  # Idee: Vielleicht Informatik > Data Science >> Statistik > Mathematik
+  mathe_probs = c(0.05, 0.05, 0.05, 0.1, 0.2, 0.3, 0.25)
+  stat_probs = c(0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2)
+  ds_probs = c(0.1, 0.1, 0.1, 0.15, 0.2, 0.2, 0.15)
+  # Versuche alles moeglichst aehnlich zu verteilen:
+    
+  ## drei Fälle haben wir hier:
+    #1_ Wenn das Individuum Mathematik studiert:
+    #2_ Wenn das Individuum Statistik studiert:
+    #3_ Wenn das Individuum Informatik/ Data Science studiert:
+  rating <- sapply(subject, function(x) {
+   if (x == "Mathematik") {
+      c(sample(1:7, 1, replace = TRUE, prob = mathe_probs),
+        sample(7:1, 1, replace = TRUE, prob = mathe_probs))
+    } else if (x == "Statistik") {
+      c(sample(1:7, 1, replace = TRUE, prob = stat_probs),
+      sample(7:1, 1, replace = TRUE, prob = stat_probs))
+    } else{
+      c(sample(1:7, 1, replace = TRUE, prob = ds_probs), 
+        sample(7:1, 1, replace = TRUE, prob = ds_probs))
+   } 
+  })
     # ab hier wiederholt sich alles
     # Die Einfluesse sind sehr leicht, muessten aber im Anschluss nachvollziebar
     # sein! (probs sind in Summe 1)
-  }
   
-  # 5 - Interesse an Programmieren Spalte:
-  # Idee: Vielleicht Informatik > Data Science >> Statistik > Mathematik
-  coding_rating <- rep("ubk", N)
-    
+  maths_rating <- as.vector(rating[1,])
+  coding_rating <- as.vector(rating[2,])
   # 6 - Mathe LK Spalte: (Ja/ Nein)
   # Idee: W'keit Mathe LK gehabt zu haben: 
-  # Mathematik > Statistik > Data Science == Informatik (aehnlich wie Spass an Mathe)
-  advanced_maths <- rep("ubk", N)
+  # zufällig für alle Studienfächer
+  advanced_maths <- sample(c(0,1), N, replace = TRUE)
   
   # Zusammenstellen zu einem data.frame:
   return(data.frame(id, age, subject, maths_rating, coding_rating, advanced_maths))
