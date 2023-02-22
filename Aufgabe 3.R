df8c9e060eda172a5dd035511641eb21803e37d5
Manasi <105052287+manasii1812@users.noreply.github.com>
2023-02-23 04:35:22 +0530

    Update Aufgabe 3.R

--- Aufgabe 3.R
+++ Aufgabe 3.R
@@ -101,57 +101,7 @@ categorical_var <- function(csvfile) {
 
 
 
-# c) 
 
-install.packages("DescTools")
-
-library(DescTools)
-
-# Kreutabelle 
-
-kreuztabelle <- xtabs(~ x + y , data=...) # x ist eine Spalte des Datensatzes und y eine andere Spalte ,data ist einfach der Datensatz
-ftable(kreuztabelle)   # Häufigkeiten
-prop.table(kreuztabelle) # Prozente
-
-# Spearman Rangkorrelation und Rangkorrelation nach Kendall (ACHTUNG: nur für ordinale Daten verwenden)
-
-cor(x,y,method = "spearman") 
-
-cor(x,y,method = "kendall")
-
-# Phi koeffizient, Kontingenzkoeffizient und Cramers V
-# ACHTUNG Phi koeffizient nur bei dichotomen Variablen,also Variablen die nur zwei Merkmale annehmen können benutzen)
-
-Phi(x,y) # Phi- Koeffizient
-
-ContCoef(x,y) # Pearsons Kontingenzkoeffizient
-
-CramerV(x,y) # Cramer V
-
-
-
-
-## Funktion
-
-my_cfunction <- function(x,y){
-  
-   a <- cor(x,y,method = "spearman") 
-   
-   b <- cor(x,y,method = "kendall")
-   
-   c <- Phi(x,y)
-   
-   d <- ContCoef(x,y)
-   
-   e <- CramerV(x,y)
-   
-   print(c(a,b,c,d,e))
-   
-}
-
-
-# manas-aufgaben
- 
 
 # c) 
 
