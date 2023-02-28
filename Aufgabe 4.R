ee200c3e707c53fbade89bbdfe0d90557f34bae1
seriadi12 <119417227+seriadi12@users.noreply.github.com>
2023-02-28 19:54:13 +0100

    Richtige Werte

--- Aufgabe 4.R
+++ Aufgabe 4.R
@@ -17,19 +17,19 @@ age <- Datensatz_Aufgabe2$age
 
 descriptive_stats(age)
 
-#       Statistics      Value
+#      Statistics      Value
 # 1           Size 100.000000
 # 2        Maximum  29.000000
-# 3        Minimum  19.000000
-# 4          Range  10.000000
-# 5           Mean  25.110000
+# 3        Minimum  20.000000
+# 4          Range   9.000000
+# 5           Mean  24.860000
 # 6         Median  25.000000
 # 7  0.75-Quartile  26.000000
 # 8  0.25-Quartile  24.000000
-# 9             SD   1.632344
+# 9             SD   1.874981
 # 10           MAD   1.482600
-# 11          Mode  26.000000
-# 12    n-Distinct   9.000000
+# 11          Mode  24.000000
+# 12    n-Distinct  10.000000
 
 
 # Deskriptive Statistiken mit funktionen aus 3 b)
